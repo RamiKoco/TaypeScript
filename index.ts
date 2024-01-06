22af73c2ec17b8d87810abe58f8caa82d73bae2e
RamiKoco <ramikoco333433@gmail.com>
2024-01-06 21:11:24 +0300

    Data Type

---
+++ index.ts
@@ -0,0 +1,41 @@
+//TypeScript nedir?
+//nesne yönelimli ve derlenebilir açık kaynaklı bir programlama dilidir.
+// let num1:number=61;
+// let num2:number=12;
+// console.log(num1*num2);
+// let city:string="Ankara";
+// console.log(city);
+
+// let bl:boolean=false;
+// if(num1<num2)
+// {
+//     console.log("Doğru "+!bl);
+// }
+// else
+// {
+//     console.log("Yanlış "+bl);
+// }
+
+// let numbers:number[]=[0,2,4,6,8,10];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+    
+// }
+
+// let names:string[]=['Rami','Koco','Hasan','Yaser','Rafet'];
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+    
+// }
+
+// let cityList:Array<string>=["Ankara",'İstanbul',"Kırıkkale",'Bolu'];
+// for (let i = 0; i < cityList.length; i++) {
+//     console.log(cityList[i]);
+    
+// }
+
+let cityList:Array<any>=["Rami Koco",'İstanbul',35000,12.4,false,'R'];
+for (let i = 0; i < cityList.length; i++) {
+    console.log(cityList[i]);
+    
+}
