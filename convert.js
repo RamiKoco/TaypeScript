de7d2939cd6788ff44c39792970b4a2046ece17c
RamiKoco <ramikoco333433@gmail.com>
2024-01-08 22:12:18 +0300

    converting

---
+++ convert.js
@@ -0,0 +1,9 @@
+var num1 = '2';
+var num2 = 3;
+var str = 55;
+console.log("--------str wiyh int ---------");
+console.log(num1 + num2);
+console.log("-------convert to int ----------");
+console.log(parseInt(num1) + num2);
+console.log("-------convert to string----------");
+console.log(num2 + str.toString());
---
+++ convert.ts
@@ -0,0 +1,10 @@
+let num1:string='2';
+let num2:number=3;
+let str:number=55;
+console.log("--------str wiyh int ---------");
+console.log(num1+num2);
+console.log("-------convert to int ----------");
+console.log(parseInt(num1)+num2);
+console.log("-------convert to string----------");
+console.log(num2+str.toString());
+
